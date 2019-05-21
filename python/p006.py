849bff5e0b297497b0458086f969190ae301512d
Xiaochen Li <jacobxiaochen@sina.com>
2019-05-21 14:15:13 -0700

    update

---
+++ python/p006.py
@@ -0,0 +1,32 @@
+#
+# Solution to Project Euler problem 6
+#
+# Author: Xiaochen Li
+# Date: May 20, 2019
+#
+#
+#
+
+def square( n ) :
+    return n * n
+
+
+
+def sum_square_diff():
+    sum_of_square = 0
+    square_of_sum = 0
+    for i in range( 100 ):
+        sum_of_square += square( i );
+        square_of_sum += i
+    square_of_sum = square(square_of_sum)
+    diff = sum_of_square - square_of_sum
+    return diff
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    print( sum_square_diff() ) 
